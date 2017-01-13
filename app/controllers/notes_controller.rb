d063b9baac286b13f45a7ebcb5123e33aeaa6cdd
StefanKollaart <garbera@icloud.com>
2017-01-13 16:41:18 +0100

    Created the index page of the notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,4 +3,8 @@ class NotesController < ApplicationController
   def index
     @notes = Note.all
   end
+
+  def show
+    @note = Note.find(params[:id])
+  end
 end
--- app/controllers/notes_controller_conflict-20170113-155716.rb
+++
@@ -1,5 +0,0 @@
-class NotesController < ApplicationController
-  def show
-    @note = Note.find(params[:id])
-  end
-end
